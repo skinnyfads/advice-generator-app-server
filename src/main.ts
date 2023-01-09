f33d1a50d79f24a522e9ffe89f910ccc1faa0465
skinnyfads <skinnyfads@proton.me>
2023-01-09 17:20:36 +0700

    feat(api): enable all cors requests

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import "dotenv/config";
 import routes from "./routes/index.js";
 
@@ -7,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 const uri = process.env.MDB_URI;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
